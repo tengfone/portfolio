c79afe4b340a8c405519092fc96c9f5734be527d
tengfone <phangtf@gmail.com>
2024-08-17 01:06:51 +0800

    Updated portfolio

--- components/HeroSplash.tsx
+++ components/HeroSplash.tsx
@@ -76,12 +76,11 @@ export default function HeroSplash(props: Props): React.ReactElement {
                             target="_blank"
                             className="text-base font-normal text-gray-600 dark:text-gray-300"
                         >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-medium" viewBox="0 0 16 16">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-medium" viewBox="0 0 16 16">
                                 <path d="M9.025 8c0 2.485-2.02 4.5-4.513 4.5A4.506 4.506 0 0 1 0 8c0-2.486 2.02-4.5 4.512-4.5A4.506 4.506 0 0 1 9.025 8m4.95 0c0 2.34-1.01 4.236-2.256 4.236S9.463 10.339 9.463 8c0-2.34 1.01-4.236 2.256-4.236S13.975 5.661 13.975 8M16 8c0 2.096-.355 3.795-.794 3.795-.438 0-.793-1.7-.793-3.795 0-2.096.355-3.795.794-3.795.438 0 .793 1.699.793 3.795" />
                             </svg>
                         </a>
                     </div>
-
 
                     <div className='cursor-pointer w-full grid place-items-center mt-24' onClick={() => props.scrollToSection("aboutme")}>
                         <div className={styles.chevron}></div>
